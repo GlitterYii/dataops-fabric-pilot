975805d45a6acca84b9ef7a4cea62a0209f1b1e8
Ruaengsiri <156504752+GlitterYii@users.noreply.github.com>
2026-09-16 09:26:17 +0700

    Committing 2 items from workspace 7a829f6b-4fd1-45c7-8c8e-b7c317816e4b

--- fabric_items/yayee/nb_yayee_sales_transform.Notebook/notebook-content.py
+++ fabric_items/yayee/nb_yayee_sales_transform.Notebook/notebook-content.py
@@ -6,7 +6,18 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "a7654c8c-f870-4bb7-8a88-fe3866b9c2a9",
+# META       "default_lakehouse_name": "lh_test_lakehouse",
+# META       "default_lakehouse_workspace_id": "7a829f6b-4fd1-45c7-8c8e-b7c317816e4b",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "a7654c8c-f870-4bb7-8a88-fe3866b9c2a9"
+# META         }
+# META       ]
+# META     }
+# META   }
 # META }
 
 # CELL ********************
