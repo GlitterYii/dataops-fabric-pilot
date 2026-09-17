5b68401a9ba1106ee628b6a58ee9843efc8c9112
Ruaengsiri <156504752+GlitterYii@users.noreply.github.com>
2026-09-17 11:43:07 +0700

    test_a

---
+++ fabric_items/nb_endpoint_test.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "07d15704-7372-4746-a6dd-28f671348a83",
+# META       "default_lakehouse_name": "lh_endpoint_test_a",
+# META       "default_lakehouse_workspace_id": "7a829f6b-4fd1-45c7-8c8e-b7c317816e4b",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "07d15704-7372-4746-a6dd-28f671348a83"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
